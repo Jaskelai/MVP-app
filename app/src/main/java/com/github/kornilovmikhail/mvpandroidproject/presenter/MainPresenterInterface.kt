7b98e36c7380a77c20c9fa90a968ed5d1bf45686
KornilovMikhail <kornilovmisha1999@gmail.com>
2019-02-25 00:18:24 +0300

    fix

--- app/src/main/java/com/github/kornilovmikhail/mvpandroidproject/presenter/MainPresenterInterface.kt
+++ app/src/main/java/com/github/kornilovmikhail/mvpandroidproject/presenter/MainPresenterInterface.kt
@@ -2,5 +2,5 @@ package com.github.kornilovmikhail.mvpandroidproject.presenter
 
 import com.github.kornilovmikhail.mvpandroidproject.data.network.model.Event
 interface MainPresenterInterface  {
-    fun getEvents(): List<Event>
+    fun getEvents()
 }
