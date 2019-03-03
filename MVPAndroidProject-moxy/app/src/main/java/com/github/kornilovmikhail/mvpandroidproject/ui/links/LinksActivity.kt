2eba556f020a7dda4d29b76fa1157de940abc951
Adelina609 <adeliachar@gmail.com>
2019-03-04 01:03:11 +0300

    add 3rd screen layout

---
+++ MVPAndroidProject-moxy/app/src/main/java/com/github/kornilovmikhail/mvpandroidproject/ui/links/LinksActivity.kt
@@ -0,0 +1,12 @@
+package com.github.kornilovmikhail.mvpandroidproject.ui.links
+
+import android.os.Bundle
+import com.arellomobile.mvp.MvpAppCompatActivity
+
+class LinksActivity : MvpAppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_links)
+    }
+}
